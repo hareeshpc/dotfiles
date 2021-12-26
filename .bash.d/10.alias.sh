d6afbd20cb73c7a66c02140f0667373a9ffe1093
Hareesh P <hareesh.puthalath@gmail.com>
2021-12-26 08:11:31 +0100

    Add fd and rg to search in hidden directories
    * fd search in hidden directories
    `fd -HI`
    
    *rg extended search:  (Disable smart search)
    `rg -uuu`

--- .bash.d/10.alias.sh
+++ .bash.d/10.alias.sh
@@ -1,4 +1,8 @@
 eval $(thefuck --alias)
 alias pls='fuck'
+# Allow fd searching in hidden directories and skip .gitignore
+alias fda='fd -IH'
+# Disable rg smart searching, same as `grep -r`
+alias rga='rg -uuu'
 alias gs='git status'
 alias machines='cat ~/.bashrc.d/12.machines_alias.rc'
